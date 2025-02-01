46ce34374f4c9914645ce28fba73f6dcf034d968
Abdelhady117 <111305193+Abdelhady117@users.noreply.github.com>
2025-02-01 04:59:32 +0200

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // a function that prints in STDOUT the string argument.
 function displayMessage(message) {
-    console.log(message); // Print the message to the console
+  console.log(message); // Print the message to the console
 }
 
 // Export function to make it available to other files
