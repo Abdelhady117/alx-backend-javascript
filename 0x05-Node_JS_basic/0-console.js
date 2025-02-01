bafec042173b0570c8afa87e43cbdbcad67b7a62
Abdelhady117 <111305193+Abdelhady117@users.noreply.github.com>
2025-02-01 04:57:47 +0200

    Create 0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// a function that prints in STDOUT the string argument.
+function displayMessage(message) {
+    console.log(message); // Print the message to the console
+}
+
+// Export function to make it available to other files
+module.exports = displayMessage;
